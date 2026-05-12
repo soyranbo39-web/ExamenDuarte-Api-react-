06d5ed8d6d566a91532fff77244395a74a05f0a3
kevinrosaldo <soy.ranbo39@gmail.com>
2026-05-12 01:21:30 -0600

    correciones de sintaxis

--- app/models/auth.py
+++ app/models/auth.py
@@ -2,11 +2,12 @@ from datetime import datetime
 from typing import Literal
 from sqlalchemy import Boolean, DateTime, Enum, String,func
 from sqlalchemy.orm import Mapped, mapped_column
+from app.core.db import Base
 
 # se debera llamas a core.db
 
 
-def User(Base):
+class User(Base):
     __tablename__ = "Users"
     
     id : Mapped [int] = mapped_column(String(50),primary_key=True)
@@ -16,6 +17,7 @@ def User(Base):
         DateTime(timezone=True),
         server_default=func.now(),
         nullable=False,
+        
     )
     
     
