03fe16d59ee7de928d656a0070c6f5cc9ed2b8ed
Abraham089 <abrahamgb274@gmail.com>
2026-05-12 12:52:11 -0600

    Variables de entorno y metodos principales

--- app/core/config.py
+++ app/core/config.py
@@ -1,8 +1,26 @@
-
 import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # DATOS PARA LA BASE DE SQLITE
 BASE_DIR = Path(__file__).resolve().parents[2]
 SQLITE_DB_PATH = BASE_DIR / "data" / "CantoEstaHermoso"
 DATABASE_URL = f"sqlite:///{SQLITE_DB_PATH}"
+
+#galleta
+AUTH_COOKIE_NAME = os.getenv("AUTH_COOKIE_NAME")
+AUTH_COOKIE_SECURE = os.getenv("AUTH_COOKIE_SECURE").lower() == "true"
+
+#token
+ALGORITHM = os.getenv("ALGORITHM")
+if not ALGORITHM:
+  raise RuntimeError("ALGORYTHM no encontrado en el entorno")
+SECRET_KEY = os.getenv("SECRET_KEY")
+if not SECRET_KEY:
+  raise RuntimeError("SECRET_KEY no encontrada en el entorno")
+AUTH_TOKEN_EXPIRE_MINUTES = int(os.getenv("AUTH_TOKEN_EXPIRE_MINUTES"))
+if not AUTH_TOKEN_EXPIRE_MINUTES:
+  raise RuntimeError("AUTH_TOKEN_EXPIRE_MINUTES no encontrada en el entorno")
+    
--- app/core/security.py
+++ app/core/security.py
@@ -1,3 +1,18 @@
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from pwdlib import PasswordHash
+import jwt
+from jwt.exceptions import InvalidTokenError
+from datetime import timedelta, datetime, timezone
+from .config import ALGORITHM, AUTH_TOKEN_EXPIRE_MINUTES, SECRET_KEY
+
+password_hash = PasswordHash.recommended()
+DUMMY_HASH = password_hash.hash("dummypassword")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+def get_password_hash(password):
+    return password_hash.hash(password)
 
 
-#Aqui deberia ir todo lo de la seguridad hash, tokens 
+def verify_password(plain_password, hashed_password):
+    return password_hash.verify(plain_password, hashed_password)
+
