f1591598251af8b83736ef180f5c5cd005b60ee8
Abraham089 <abrahamgb274@gmail.com>
2026-05-13 10:49:03 -0600

    Modificaciones al main

--- app/main.py
+++ app/main.py
@@ -1,8 +1,11 @@
 from fastapi import FastAPI
-from fastapi.concurrency import asynccontextmanager
 from fastapi.middleware.cors import CORSMiddleware
 from dotenv import load_dotenv
 import os
+from app.api.v1.router import router as auth_router
+from app.core.config import DATABASE_URL
+from app.core.db import Base, engine
+from fastapi.staticfiles import StaticFiles
 
 load_dotenv()
 localhost_url = os.getenv("LOCALHOST")
@@ -13,32 +16,6 @@ origins = [
     ip_direction_url
 ]
 
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    create_db_and_tables()
-    yield
-
-app = FastAPI(lifespan=lifespan, separate_input_output_schemas=False)
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-import os
-
-from dotenv import load_dotenv
-from fastapi import FastAPI
-from fastapi.staticfiles import StaticFiles
-
-from app.api.v1.router import router as auth_router
-from app.core.config import DATABASE_URL
-from app.core.db import Base, engine
-
-load_dotenv()
-
 def create_app() ->FastAPI: 
     app = FastAPI(title="Examen Canto es hermoso",version="1.0.0")
     Base.metadata.create_all(bind=engine)
@@ -47,3 +24,15 @@ def create_app() ->FastAPI:
     return app
 
 app = create_app()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+
+
