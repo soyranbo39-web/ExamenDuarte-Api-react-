453d48edfd30c5e4794b75959ba8c3df101dffab
kevinrosaldo <soy.ranbo39@gmail.com>
2026-05-13 18:41:53 -0600

    Adecuacion a lo solicitado
    Se cambio los modelos pydantic y se adecuo el login

--- app/api/v1/schemas.py
+++ app/api/v1/schemas.py
@@ -1,40 +1,50 @@
 #Modelos pydantic 
 #Tenemos pensado que la cokkie dure una cantidad de tiempo 
-from pydantic import BaseModel, Field
+from typing import Literal
+from pydantic import BaseModel, ConfigDict, Field
 
-#Aqui estuvo canto 
 
-class Registro (BaseModel):
-    name : str = Field(min_length=1,max_length=100)
+#Aqui estuvo canto. Ya canto se vino a qui 
+
+class Registro(BaseModel):
+    full_name: str = Field(min_length=1,max_length=100)
     username : str =  Field(min_length=1,max_length=50)
     password : str = Field(min_length=1, max_length= 100)
+    model_config = ConfigDict(from_attributes=True)
+    
+    
+class Login(Registro):
+    id : int 
+    is_active : bool
 
-class Login(BaseModel):
-    username : str
-    password : str 
+class TokenResponde(BaseModel):
+    access_token: str
+    token_type: str = "bearer"
+    user : Login
+
 
 class CokiesOut(BaseModel):
-    name: str
-    http_only: bool
-    secure: bool
-    same_site: str
-    max_age_seconds: int
- 
-class AuthOut(BaseModel):
-     access_token : str
-     token_type : str = "bearer"
-     cookie : CokiesOut
-     
-class UserOut(BaseModel):
-    id : str
-    username : str
-    username : str
+    access_token: str
+    token_type: Literal["bearer"] = "bearer"
+      
+
+class HeaderOut(BaseModel):
+    authorization: str = Field(..., alias="Authorization")
+    model_config = ConfigDict(populate_by_name=True)
+
+class LoginIdentificadorOut(BaseModel):
+    header: HeaderOut
+    cookie: CokiesOut
+
+class LoginResponde(BaseModel):
+    body: TokenResponde
+    transports : LoginIdentificadorOut
+    
 
 class SessionOut(BaseModel):
     user_id: str
     authenticated: bool
     authenticated_via_cookie: bool
     cookie_name: str
-
     
 
